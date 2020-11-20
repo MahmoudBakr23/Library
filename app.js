2da7a4614753d1a2330b9139335b97416490ed18
Mahmoud Bakr <mbakr6821@gmail.com>
2020-11-20 16:02:13 +0200

    improving the code

--- app.js
+++ app.js
@@ -2,59 +2,69 @@ function Book(title, author, pages) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.status = 'not read';
+	this.status = 'read';
 }
 
+const library = [];
+
 function addBookToLibrary(book) {
-	const library = [];
 	library.push(book);
-	library.forEach((book) => {
-		const booksContainer = document.getElementById('list');
-		const bookRow = document.createElement('tr');
-		bookRow.innerHTML = `
-			<td>${book.title}</td>
-			<td>${book.author}</td>
-			<td>${book.pages}</td>
-			<td><button class="status">${book.status}</button</td>
-			<td><button class="delete">X</button></td>
-		`;
-		booksContainer.appendChild(bookRow);
+}
+
+function displayBooks() {
+	const booksList = library;
+	const booksContainer = document.getElementById('list');
+	booksContainer.innerHTML = '';
+	if (booksList !== null) {
+		booksList.forEach((book, index) => {
+			const bookRow = document.createElement('tr');
+			bookRow.innerHTML = `
+				<td>${book.title}</td>
+				<td>${book.author}</td>
+				<td>${book.pages}</td>
+				<td><button class="${book.status ? 'unread' : 'read'}" data-status=${index}>${book.status ? 'Unread' : 'Read'}</button</td>
+				<td><button class="delete" data-id=${index}>X</button></td>
+			`;
+			booksContainer.appendChild(bookRow);
+		});
+	}
+}
+
+function bookStatus(index) {
+	library[index].status = !library[index].status;
+	displayBooks();
+}
+
+function deleteBook(index) {
+	library.splice(index, 1);
+	displayBooks();
+}
+
+function eventListeners() {
+	document.getElementById('add-book').addEventListener('submit', (book) => {
+		book.preventDefault();
+
+		const title = document.getElementById('title').value;
+		const author = document.getElementById('author').value;
+		const pages = document.getElementById('pages').value;
+
+		const newBook = new Book(title, author, pages);
+		addBookToLibrary(newBook);
+		displayBooks();
+		document.getElementById('add-book').reset();
+	});
+
+	document.getElementById('list').addEventListener('click', (book) => {
+		const bookId = book.target.getAttribute('data-id');
+		const bookStatusId = book.target.getAttribute('data-status');
+		if (bookId !== null) {
+			deleteBook(bookId);
+		}
+		if (bookStatusId !== null) {
+			bookStatus(bookStatusId);
+		}
 	});
 }
 
-function bookStatus(book) {
-	if (book.classList.contains('status')) {
-		book.innerHTML = 'read!';
-		book.classList.add('read');
-	}
-}
-
-function deleteBook(book) {
-	if (book.classList.contains('delete')) {
-		book.parentElement.parentElement.remove();
-	}
-}
-
-const book1 = new Book("Harry Potter and the Sorcerer's Stone", 'J.K. Rowling', 800);
-const book2 = new Book('Twilight', "I Don't Remember", 500);
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-
-document.getElementById('add-book').addEventListener('submit', (book) => {
-	book.preventDefault();
-
-	const title = document.getElementById('title').value;
-	const author = document.getElementById('author').value;
-	const pages = document.getElementById('pages').value;
-
-	const newBook = new Book(title, author, pages);
-	addBookToLibrary(newBook);
-});
-
-document.getElementById('list').addEventListener('click', (book) => {
-	deleteBook(book.target);
-});
-
-document.getElementById('list').addEventListener('click', (book) => {
-	bookStatus(book.target);
-});
+displayBooks();
+eventListeners();
